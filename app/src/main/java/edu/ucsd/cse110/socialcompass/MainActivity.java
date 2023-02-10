852017a034c919c89c7da70d7059818e03583222
kleinfour <shawnwilliambowser@gmail.com>
2023-02-10 09:02:30 -0800

    loadProfile and initNewUser

--- app/src/main/java/edu/ucsd/cse110/socialcompass/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/socialcompass/MainActivity.java
@@ -1,8 +1,13 @@
 package edu.ucsd.cse110.socialcompass;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Pair;
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadProfile();
+    }
+
+    public void loadProfile() {
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+
+        // check if this is a new user, and if so, initialize their sharedPreferences
+        Boolean newUser = preferences.getBoolean("newUser", true);
+        if (newUser) { initNewUser(); }
+
+    }
+
+    // This method should only be called one time EVER - for initializing brand new users.
+    private void initNewUser() {
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        //TODO: Ask for location permission and build initial HashMap for storing data
+
     }
 }
