62939b1c8a223fe67e51807cc8a085e82306a4ec
jaysunl <74435747+jaysunl@users.noreply.github.com>
2023-02-13 10:07:04 -0800

    fix R package

--- app/src/main/java/edu/ucsd/cse110/cse110_project/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_project/MainActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import edu.ucsd.cse110.socialcompass.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
     }
 }
