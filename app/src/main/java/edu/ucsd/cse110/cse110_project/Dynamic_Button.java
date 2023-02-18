62b02be75dc4c1f8f2cdb92cde8c7606b3882290
Gordon Ling <goling@ucsd.edu>
2023-02-17 21:00:37 -0800

    Created Dynamic Button class and working on real time updating

--- app/src/main/java/edu/ucsd/cse110/cse110_project/Dynamic_Button.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_project/Dynamic_Button.java
@@ -1,5 +1,7 @@
 package edu.ucsd.cse110.cse110_project;
 
+import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -10,22 +12,96 @@ import androidx.core.content.ContextCompat;
 import java.util.HashMap;
 
 public class Dynamic_Button {
-    int angle;
+    private float bearingAngle;
+    private Context context;
     static final int radius = 495; //some hard-coded value
-    String label;
+    private String label;
+    private TextView button;
 
-    Pair<Double, Double> loc; //(Long, Lat) or was it (Lat, Long)?
 
-    public Dynamic_Button(Pair<Double, Double> curr, Pair<Double, Double> dest, String label) {
-
+    public Dynamic_Button(Context context, float bearingAngle) {
+        this.context = context;
+        this.bearingAngle = bearingAngle;
     }
 
-    public void updateAngle(Pair<Double, Double> curr){
-
+    public void updateAngle(float bearingAngle) {
+        this.bearingAngle = bearingAngle;
     }
 
+    public float getBearingAngle() {
+        return bearingAngle;
+    }
+
+    public TextView getButton() {
+        return button;
+    }
+
+    public void createButton() {
+        button = new TextView(context);
+        button.setId(View.generateViewId());
+        button.setBackground(ContextCompat.getDrawable(context, R.drawable.house_icon));
+        ConstraintLayout.LayoutParams layout = new ConstraintLayout.LayoutParams(
+                150, 150
+        );
+        layout.circleRadius = radius;
+        layout.circleConstraint = R.id.location_icon;
+        layout.circleAngle = bearingAngle;
+        layout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
+        button.setLayoutParams(layout);
+    }
+
+    private void updateButtonLayout() {
+        ConstraintLayout.LayoutParams layout = (ConstraintLayout.LayoutParams) button.getLayoutParams();
+        layout.circleAngle = bearingAngle;
+        button.setLayoutParams(layout);
+    }
+}
+
+/*
+public class Dynamic_Button {
+    private float bearingAngle;
+    private Context context;
+    static final int radius = 495; //some hard-coded value
+    private String label;
+
+    //Pair<Double, Double> loc; //(Long, Lat) or was it (Lat, Long)?
+
+    public Dynamic_Button(Context context, float bearingAngle) {
+        this.context = context;
+        this.bearingAngle = bearingAngle;
+        //this.label = label;
+    }
+
+    public void updateAngle(float new_angle){
+        this.bearingAngle = new_angle;
+    }
+
+/*
     public displayLabel(){
 
+    }*/
+/*
+    public void createButton() {
+        TextView sample = new TextView(context);
+        sample.setId(View.generateViewId());
+        sample.setBackground(ContextCompat.getDrawable(context, R.drawable.house_icon));
+        ConstraintLayout.LayoutParams layout = new ConstraintLayout.LayoutParams(
+                150, 150
+        );
+        layout.circleRadius = 490;
+        layout.circleConstraint = R.id.location_icon;
+        layout.circleAngle = bearingAngle;
+        layout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
+        sample.setLayoutParams(layout);
+
+        ViewGroup mainLayout = (ViewGroup) ((Activity) context).findViewById(R.id.main_layout);
+        mainLayout.addView(sample);
     }
 
 }
--- app/src/main/java/edu/ucsd/cse110/cse110_project/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_project/MainActivity.java
@@ -13,6 +13,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,12 +21,16 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<Dynamic_Button> dynamic_buttons;
     private LocationService locationService;
-    private LocationService mockCoordinates;
-
-
+    //private double geiselLat = 32.88114549458315d;
+    //private double geiselLong = -117.23758450131251d;
+    private HashMap<String, Pair<Double,Double>> sampleHashSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +42,84 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},200);
         }
 
+        //Populating hashmap for quick reference
+        sampleHashSet = new HashMap<String, Pair<Double,Double>>();
+        sampleHashSet.put("Geisel", new Pair<>(32.88114549458315d,-117.23758450131251d ));
+        sampleHashSet.put("Rimac", new Pair<>(32.885159942166624d, -117.24044656136009d ));
+
+
         locationService = LocationService.singleton(this);
         TextView textView = (TextView) findViewById(R.id.serviceTextView);
+        TextView bearingAngle = (TextView) findViewById(R.id.bearingAngle);
 
-        double geiselLat = 32.88114549458315d;
-        double geiselLong = -117.23758450131251d;
+        // Create a list to hold ButtonCreator objects
+        dynamic_buttons = new ArrayList<>();
 
-        TextView bearing_angle = (TextView) findViewById(R.id.bearingAngle);
-        TextView houseIcon = (TextView) findViewById(R.id.house_icon);
+        // Instantiate ButtonCreator objects and add them to the list
+//        dynamic_buttons.add(new Dynamic_Button(this, 0f));
+//        dynamic_buttons.add(new Dynamic_Button(this, 90f));
+
 
 
         locationService.getLocation().observe(this, loc->{
             textView.setText(Double.toString(loc.first)+" , "+
                     Double.toString(loc.second));
-            double angle = Bearing.bearing(loc.first, loc.second, geiselLat, geiselLong);
-            bearing_angle.setText(Double.toString(angle));
-            //createButtons((int)angle);
+            //double angle = Bearing.bearing(loc.first, loc.second, geiselLat, geiselLong);
+            //bearingAngle.setText(Double.toString(angle));
 
+            // Iterate through the ButtonCreator objects, update their bearing angles,
+            // and display them in the layout
+
+            for(String i : sampleHashSet.keySet()){
+                double angle = Bearing.bearing(loc.first, loc.second, sampleHashSet.get(i).first, sampleHashSet.get(i).second);
+                dynamic_buttons.add(new Dynamic_Button(this, (float)angle));
+
+
+            }
+
+            for (Dynamic_Button button : dynamic_buttons) {
+                //buttonCreator.updateAngle((float) 120);
+
+                //buttonCreator.updateAngle();
+                ConstraintLayout mainLayout = findViewById(R.id.main_layout);
+                mainLayout.removeView(button.getButton());
+                button.createButton();
+                mainLayout.addView(button.getButton());
+            }
         });
+    }
 
-    }
-    }
+//    // Assume that we have a method called startPolling that starts the polling process
+//    public void startPolling() {
+//        final Handler handler = new Handler();
+//
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                float newBearingAngle = getBearingAngle(); // Get the latest bearing angle
+//                if (newBearingAngle != Dynamic_Button.getBearingAngle()) {
+//                    // Update the button position if the bearing angle has changed
+//                    Dynamic_Button.setBearingAngle(newBearingAngle);
+//                    Dynamic_Button.updateButtonLayout();
+//                }
+//                handler.postDelayed(this, POLL_INTERVAL); // Schedule the next check
+//            }
+//        };
+//
+//        handler.postDelayed(runnable, POLL_INTERVAL); // Start the polling process
+//    }
+//
+//    private float getBearingAngle() {
+//        // Return the latest bearing angle
+//    }
+
+    private static final long POLL_INTERVAL = 1000; // Poll every 1 second
+
 }
+
+
+
+
+
+
+
