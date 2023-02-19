32b577ecd223a9ca72a552c54c5f8c4509a7aa6e
swu3025 <65374120+swu3025@users.noreply.github.com>
2023-02-18 16:23:20 -0800

    rotated the NESW signs

--- app/src/main/java/edu/ucsd/cse110/cse110_project/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_project/MainActivity.java
@@ -1,10 +1,14 @@
 package edu.ucsd.cse110.cse110_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 import androidx.core.util.Pair;
 import androidx.core.app.ActivityCompat;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.jar.Manifest;
@@ -44,7 +48,40 @@ public class MainActivity extends AppCompatActivity {
 
     private void onOrientationChanged(Float orientation) {
         TextView orientationText = findViewById(R.id.serviceTextView);
-        orientationText.setText(Utilities.formatOrientation(orientation));
+        float degrees = (float) Math.toDegrees(orientation);
+        if (1 / degrees == Double.POSITIVE_INFINITY) degrees = 180;
+        if (degrees < 0) {
+            degrees += 360;
+        }
+        orientationText.setText(Utilities.formatOrientation(degrees));
+
+        // rotating the NESW signs
+        TextView northView = (TextView) findViewById(R.id.north);
+        TextView eastView = (TextView) findViewById(R.id.east);
+        TextView southView = (TextView) findViewById(R.id.south);
+        TextView westView = (TextView) findViewById(R.id.west);
+        float north_angle = 360 - degrees;
+        float east_angle = 450 - degrees;
+        float south_angle = 180 - degrees;
+        float west_angle = 270 - degrees;
+        setLayout(north_angle,northView);
+        setLayout(east_angle,eastView);
+        setLayout(south_angle,southView);
+        setLayout(west_angle,westView);
+    }
+
+    private void setLayout(float angle, TextView view){
+        ConstraintLayout.LayoutParams layout = new ConstraintLayout.LayoutParams(
+                55, 100
+        );
+        layout.circleRadius = 495;
+        layout.circleConstraint = R.id.location_icon;
+        layout.circleAngle = angle;
+        layout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID;
+        layout.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
+        view.setLayoutParams(layout);
     }
 
     @Override
--- app/src/main/java/edu/ucsd/cse110/cse110_project/Utilities.java
+++ app/src/main/java/edu/ucsd/cse110/cse110_project/Utilities.java
@@ -5,12 +5,7 @@ import java.util.Locale;
 public class Utilities {
 
     static String formatOrientation(float azimuth) {
-        float degrees = (float) Math.toDegrees(azimuth);
-        if (1 / degrees == Double.POSITIVE_INFINITY) degrees = 180;
-        if (degrees < 0) {
-            degrees += 360;
-        }
         //return String.format( "%.0f degrees from North", degrees);
-        return String.format(Locale.US, "%.0f degrees from North", degrees);
+        return String.format(Locale.US, "%.0f degrees from North", 360-azimuth);
     }
 }
