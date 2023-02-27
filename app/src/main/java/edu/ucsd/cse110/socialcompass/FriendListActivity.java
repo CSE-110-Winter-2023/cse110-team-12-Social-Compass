e16244d3e20cfd37ae1c2e4f7387629b867d53a9
kleinfour <shawnwilliambowser@gmail.com>
2023-02-26 19:25:12 -0800

    minor changes to fix build

--- app/src/main/java/edu/ucsd/cse110/socialcompass/FriendListActivity.java
+++ app/src/main/java/edu/ucsd/cse110/socialcompass/FriendListActivity.java
@@ -1,25 +1,30 @@
 package edu.ucsd.cse110.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class FriendListActivity extends AppCompatActivity {
-
     public RecyclerView recyclerView;
+    private FriendListViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
-    }
 
-//    FriendsListAdapter adapter = new FriendsListAdapter();
-//    adapter.setHasStableIds(true);
-//
-//    recyclerView = findViewById(R.id.friend_items);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setAdapter(adapter);
-//
-//        adapter.setLocationListItems(FriendsListItem.loadJSON(this, "test_friends.json"));
+        viewModel = new ViewModelProvider(this)
+                .get(FriendListViewModel.class);
+
+        FriendListAdapter adapter = new FriendListAdapter();
+        adapter.setHasStableIds(true);
+        viewModel.getFriendListItems().observe(this, adapter::setFriendListItems);
+
+        recyclerView = findViewById(R.id.friend_items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
 }
