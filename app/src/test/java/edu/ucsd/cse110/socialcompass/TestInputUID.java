3d8fd02dd078593b3d644145e3b1219ae1738173
kleinfour <shawnwilliambowser@gmail.com>
2023-03-04 15:00:28 -0800

    initial commit

---
+++ app/src/test/java/edu/ucsd/cse110/socialcompass/TestInputUID.java
@@ -0,0 +1,15 @@
+package edu.ucsd.cse110.socialcompass;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.Objects;
+
+public class TestInputUID {
+    @Test
+    void testInput1() {
+
+    }
+}
