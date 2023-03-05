99df053ea366785d6bb1764dd634a6bb4926d087
Gordon Ling <goling@ucsd.edu>
2023-03-05 14:42:16 -0800

    Trying to get the add button to add EditText to Database

--- app/src/main/java/edu/ucsd/cse110/socialcompass/FriendListActivity.java
+++ app/src/main/java/edu/ucsd/cse110/socialcompass/FriendListActivity.java
@@ -7,10 +7,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FriendListActivity extends AppCompatActivity {
     public RecyclerView recyclerView;
     private FriendListViewModel viewModel;
+    private EditText uidEditText;
+    private Button addButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +32,25 @@ public class FriendListActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.friend_items);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+
+        uidEditText = findViewById(R.id.UID_text);
+        addButton = findViewById(R.id.add_btn);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uid = uidEditText.getText().toString();
+                insertFriendListItem(uid);
+                uidEditText.setText("");
+            }
+        });
     }
+
+    public void insertFriendListItem(String uid) {
+        FriendDatabase db = FriendDatabase.getSingleton(getApplication());
+        FriendListItemDao dao = db.friendListItemDao();
+        int order = dao.getOrderForAppend();
+        dao.insert(new FriendListItem(uid, "Unknown", order));
+    }
+
 }
