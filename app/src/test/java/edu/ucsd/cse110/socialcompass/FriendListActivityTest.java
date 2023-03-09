85529edfdf16ac8f90e7f631b4c862e662c44a14
swu3025 <65374120+swu3025@users.noreply.github.com>
2023-03-08 22:35:19 -0800

    Added a third test

--- app/src/test/java/edu/ucsd/cse110/socialcompass/FriendListActivityTest.java
+++ app/src/test/java/edu/ucsd/cse110/socialcompass/FriendListActivityTest.java
@@ -4,6 +4,8 @@ import androidx.room.Room;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+
 
 import org.junit.After;
 import org.junit.Before;
@@ -29,14 +31,24 @@ public class FriendListActivityTest {
 
     private FriendListItemDao dao;
     private FriendDatabase db;
+    ActivityScenario<MainActivity> mainScenario;
+    ActivityScenario<FriendListActivity> friendListScenario;
+
+
 
     @Before
-    public void createDb() {
+    public void init() {
         Context context = ApplicationProvider.getApplicationContext();
+
         db = Room.inMemoryDatabaseBuilder(context, FriendDatabase.class)
                 .allowMainThreadQueries()
                 .build();
         dao = db.friendListItemDao();
+
+        FriendDatabase.useTestSingleton(getApplicationContext());
+
+        mainScenario = ActivityScenario.launch(MainActivity.class);
+        friendListScenario = ActivityScenario.launch(FriendListActivity.class);
     }
 
     @After
@@ -46,12 +58,8 @@ public class FriendListActivityTest {
 
     @Test
     public void testPromptUserForUid() {
-        // Launch activity
-        ActivityScenario.launch(MainActivity.class);
-        ActivityScenario<FriendListActivity> scenario = ActivityScenario.launch(FriendListActivity.class);
-
         // Set UID during activity
-        scenario.onActivity(activity -> {
+        friendListScenario.onActivity(activity -> {
             String uid = "testUid";
             activity.uidEditText.setText(uid);
             assertEquals(uid, activity.uidEditText.getText().toString());
@@ -71,5 +79,17 @@ public class FriendListActivityTest {
 
         assertEquals(1, items.size());
         assertEquals(uid, items.get(0).getUid());
+    }
+
+    @Test
+    public void testShowUserPromptAlert() {
+
+        // Show user prompt alert
+        friendListScenario.onActivity(activity -> {
+            //Making sure dialog appears
+            AlertDialog alertDialog = ShadowAlertDialog.getLatestAlertDialog();
+            assertNotNull(alertDialog);
+            assertTrue(alertDialog.isShowing());
+        });
     }
 }
