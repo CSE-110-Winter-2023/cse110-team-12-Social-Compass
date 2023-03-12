8e0ee6a53b72c5f7a6654a52e5dba2f855efecd0
swu3025 <65374120+swu3025@users.noreply.github.com>
2023-03-11 17:30:12 -0800

    included zone radius into Constants file

--- app/src/main/java/edu/ucsd/cse110/socialcompass/Constants.java
+++ app/src/main/java/edu/ucsd/cse110/socialcompass/Constants.java
@@ -1,7 +1,19 @@
 package edu.ucsd.cse110.socialcompass;
 public class Constants {
-    public static final float NORTH = 0;
-    public static final float EAST = (float) (Math.PI / 2);
-    public static final float SOUTH = (float) Math.PI;
-    public static final float WEST = (float) (3 * Math.PI / 2);
+    // increment by 39 (except for ZONE1_2 since it is too close to the location_icon)
+    public static final float ZONE1_1 = 0; // 0
+    public static final float ZONE1_2 = 42; // (0.2,0.4]
+    public static final float ZONE1_3 = 78;  // (0.2,0.4]
+    public static final float ZONE1_4 = 117; // (0.4,0.6]
+    public static final float ZONE1_5 = 156; // (0.6,0.8]
+
+    public static final float ZONE2_1 = 195; // (0.8, 1]
+    public static final float ZONE2_2 = 234; // (1.0,2.8]
+    public static final float ZONE2_3 = 273; // (2.8,4.6]
+    public static final float ZONE2_4 = 312; // (4.6,6.4]
+    public static final float ZONE2_5 = 351; // (6.4, 8.2]
+
+    public static final float ZONE3_1 = 390; // (8.2, 10]
+
+
 }
