24411b6d5d58b84bd6d3289403ad3399917149f4
kleinfour <shawnwilliambowser@gmail.com>
2023-03-12 16:07:25 -0700

    added calculateDistance Utility function

--- app/src/main/java/edu/ucsd/cse110/socialcompass/Utilities.java
+++ app/src/main/java/edu/ucsd/cse110/socialcompass/Utilities.java
@@ -178,4 +178,28 @@ public class Utilities {
                 Math.abs(latitude), Math.abs(latitude % 1) * 60, Math.abs(latitude % 1 % 1) * 60,
                 Math.abs(longitude), Math.abs(longitude % 1) * 60, Math.abs(longitude % 1 % 1) * 60);
     }
+
+    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        // convert to radians
+        lon1 = Math.toRadians(lon1);
+        lon2 = Math.toRadians(lon2);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        // Haversine formula
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers. Use 3956
+        // for miles
+        double r = 6371;
+
+        // calculate the result
+        return(c * r);
+    }
 }
