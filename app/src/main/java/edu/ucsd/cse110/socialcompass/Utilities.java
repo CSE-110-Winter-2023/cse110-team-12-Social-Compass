48d4c7da66551da2beb898f2ea879b3c0982d4f0
jaysunl <74435747+jaysunl@users.noreply.github.com>
2023-03-14 11:54:16 -0700

    fix distance updating and add to database

--- app/src/main/java/edu/ucsd/cse110/socialcompass/Utilities.java
+++ app/src/main/java/edu/ucsd/cse110/socialcompass/Utilities.java
@@ -179,30 +179,6 @@ public class Utilities {
                 Math.abs(longitude), Math.abs(longitude % 1) * 60, Math.abs(longitude % 1 % 1) * 60);
     }
 
-    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        // convert to radians
-        lon1 = Math.toRadians(lon1);
-        lon2 = Math.toRadians(lon2);
-        lat1 = Math.toRadians(lat1);
-        lat2 = Math.toRadians(lat2);
-
-        // Haversine formula
-        double dlon = lon2 - lon1;
-        double dlat = lat2 - lat1;
-        double a = Math.pow(Math.sin(dlat / 2), 2)
-                + Math.cos(lat1) * Math.cos(lat2)
-                * Math.pow(Math.sin(dlon / 2),2);
-
-        double c = 2 * Math.asin(Math.sqrt(a));
-
-        // Radius of earth in kilometers. Use 3956
-        // for miles
-        double r = 3956;
-
-        // calculate the result
-        return(c * r);
-    }
-
     public static String getName() {
         return uniqueName;
     }
